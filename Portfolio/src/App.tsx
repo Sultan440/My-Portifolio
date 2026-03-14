5e9f84374891f544bc0f565dc615dc1c94051722
Sultan440 <vmutswinya@gmail.com>
2026-03-14 11:41:07 -0700

    creating a react app for the site

---
+++ Portfolio/src/App.tsx
@@ -0,0 +1,10 @@
+import Hello from "./assets/home";
+
+function App(){
+  return(
+    <main>
+      <Hello/>
+    </main>
+  )
+}
+export default App;
---
+++ Portfolio/src/assets/home.tsx
@@ -0,0 +1,7 @@
+function Hello(){
+    return(
+        <h1>My Portfolio Website</h1>
+    )
+}
+
+export default Hello;
